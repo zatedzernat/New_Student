f57ce75fba0d1882d3d8247d521e117c1b73f216
BILL-ROG <bccbew164@gmail.com>
2019-07-12 14:32:24 +0700

    change script opcl

--- opcldate.sql
+++ opcldate.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE `opcldate` (
- `opendate` date NOT NULL,
- `closedate` date NOT NULL,
+ `opendate` datetime NOT NULL,
+ `closedate` datetime NOT NULL,
  `semester` varchar(10) NOT NULL COMMENT 'ex "1/2562"',
  PRIMARY KEY (`semester`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
