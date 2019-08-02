9c16383d9ab42a81d7ea2c9a29d22b31496d878b
zatedzernat <bccbew164@gmail.com>
2019-08-03 01:34:22 +0700

    add bg imag to all page

--- New_StudentAPP/resources/views/master.blade.php
+++ New_StudentAPP/resources/views/master.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title')</title>
-    
+
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
@@ -22,31 +22,47 @@
 
     @yield('header')
     <style>
+        body,
+        html {
+            height: 100%;
+            margin: 0;
+        }
+
         body {
             background-color: gray;
+        }
+
+        .sit-img {
+            background-image: url('images/sit1.jpg');
+            background-position: center;
+            background-repeat: no-repeat;
+            height: 100%;
+            background-size: cover;
         }
     </style>
 </head>
 
 <body>
-    <br>
-    <div class="container">
-        <div class="row justify-content-center">
-            <TABLE WIDTH=816 BORDER=0 CELLPADDING=0 CELLSPACING=0 align="center">
-                <TR>
-                    <TD>
-                        <IMG SRC="images/head_new_msc_01.jpg" WIDTH=224 HEIGHT=150 ALT="" class="img-fluid"></TD>
-                    <TD>
-                        <IMG SRC="images/head_new_msc_02.jpg" WIDTH=268 HEIGHT=150 ALT="" class="img-fluid"></TD>
-                    <TD>
-                        <IMG SRC="images/head_new_msc_03.jpg" WIDTH=181 HEIGHT=150 ALT="" class="img-fluid"></TD>
-                    <TD>
-                        <IMG SRC="images/head_new_msc_04.jpg" WIDTH=143 HEIGHT=150 ALT="" class="img-fluid"></TD>
-                </TR>
-            </TABLE>
+    <div class="sit-img">
+        <br>
+        <div class="container">
+            <div class="row justify-content-center">
+                <TABLE WIDTH=816 BORDER=0 CELLPADDING=0 CELLSPACING=0 align="center">
+                    <TR>
+                        <TD>
+                            <IMG SRC="images/head_new_msc_01.jpg" WIDTH=224 HEIGHT=150 ALT="" class="img-fluid"></TD>
+                        <TD>
+                            <IMG SRC="images/head_new_msc_02.jpg" WIDTH=268 HEIGHT=150 ALT="" class="img-fluid"></TD>
+                        <TD>
+                            <IMG SRC="images/head_new_msc_03.jpg" WIDTH=181 HEIGHT=150 ALT="" class="img-fluid"></TD>
+                        <TD>
+                            <IMG SRC="images/head_new_msc_04.jpg" WIDTH=143 HEIGHT=150 ALT="" class="img-fluid"></TD>
+                    </TR>
+                </TABLE>
+            </div>
         </div>
+        @yield('content')
     </div>
-    @yield('content')
 </body>
 
 </html>
